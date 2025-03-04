c8b8c3375f786d7753acb312decae160e8671583
mehedi hasan <mhasanwk@gmail.com>
2025-03-04 03:21:00 -0500

    numbers added

---
+++ src/repoprac/repoprac.java
@@ -0,0 +1,11 @@
+package repoprac;
+
+public class repoprac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the first hw for practice both in eclipse and github");
+System.out.println(1-2);
+	}
+
+}
